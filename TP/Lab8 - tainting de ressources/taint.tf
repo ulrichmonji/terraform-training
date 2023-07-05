d8dc3990121cc5a7fba34a5403f3010f6ad32642
Ulrich MONJI <ulrich_nathan@yahoo.fr>
2023-07-05 14:57:45 +0200

    add second resource on LAB 8

--- TP/Lab8 - tainting de ressources/taint.tf
+++ TP/Lab8 - tainting de ressources/taint.tf
@@ -15,3 +15,16 @@ resource "aws_instance" "myec2" {
     delete_on_termination = true
   }
 }
+
+
+resource "aws_instance" "myec2_2" {
+  ami           = "ami-0715c1897453cabd1"
+  instance_type = "t2.micro"
+  key_name      = "terraform"
+  tags = {
+    Name = "ec2-ulrich-NEW"
+  }
+  root_block_device {
+    delete_on_termination = true
+  }
+}
